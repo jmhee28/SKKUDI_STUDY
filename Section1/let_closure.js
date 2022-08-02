3fb09f038accaa6d42f964188d4011cc40751f6d
jmhee28 <mhee3428@gmail.com>
2022-08-02 18:41:43 +0900

    8-2

---
+++ Section1/let_closure.js
@@ -0,0 +1,8 @@
+var list = document.querySelectorAll("li");
+for(var i=0; i<list.length; i++){
+    list[i].addEventListener("click", function(){
+      console.log(i+"번째 리스트 입니다"); // i 는 closure 변수 
+     // i가 밖에 있는 i값과 참조를 계속 함
+      // => 마지막 값인 4를 공유하면서 생기는 문제
+    });
+  }
